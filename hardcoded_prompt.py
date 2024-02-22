51c40112068c7a5a8e335e2f109179f7c454e053
Ivan Osipov <ivan.osipov@valagroup.com>
2024-02-22 18:35:23 +0200

    fix the excel visibility outcome

--- hardcoded_prompt.py
+++ hardcoded_prompt.py
@@ -8,25 +8,25 @@ from mistralai.models.chat_completion import ChatMessage
 
 def main():
     api_key = os.environ["MISTRAL_API_KEY"]
-    #model = "mistral-small"
+    model = "mistral-small"
     #model = "mistral-tiny"
-    model = "mistral-medium"  # the best one.
+    #model = "mistral-medium"  # the best one.
 
     client = MistralClient(api_key=api_key)
     sut = "e-mail restore"
     sut_elements = ["email field", "send button"]
     testing_types = ["functional", "usability", "compatibility"]
     columns = ["number", "short description", "priority", "testing type", "steps", "expected results"]
-    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Without greetings etc, just the test cases list. Outcome should be in a .csv file format. Columns: {columns}. One step is one action."
+    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Outcome should be ready for instant convertation to the csv file. Don't add any other text. Use different testing types, for example {testing_types}. Columns: {columns}. One step is one action."
 
     chat_response = client.chat(
         model=model,
         messages=[ChatMessage(role="user", content=prompt)],
     )
-    cases = chat_response.choices[0].message.content
+    cases = chat_response.choices[0].message.content[1:-1]  # remove " symbols
     print(cases)
-    with open("test_cases_3.csv", "w") as file:
-        file.write("sep=\n")  # User-friendly Excel
+    with open("test_cases.csv", "w") as file:
+        file.write("sep=,\n")  # User-friendly Excel
         file.write(cases)
 
 if __name__ == "__main__":
