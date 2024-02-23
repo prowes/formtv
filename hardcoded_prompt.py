3d2a9d2aef630ab9537bc550ca12748f221cd181
Ivan Osipov <ivan.osipov@valagroup.com>
2024-02-23 12:30:14 +0200

    More files

--- hardcoded_prompt.py
+++ hardcoded_prompt.py
@@ -8,16 +8,16 @@ from mistralai.models.chat_completion import ChatMessage
 
 def main():
     api_key = os.environ["MISTRAL_API_KEY"]
-    model = "mistral-small"
+    #model = "mistral-small"
     #model = "mistral-tiny"
-    #model = "mistral-medium"  # the best one.
+    model = "mistral-medium"  # the best one.
 
     client = MistralClient(api_key=api_key)
     sut = "e-mail restore"
     sut_elements = ["email field", "send button"]
-    testing_types = ["functional", "usability", "compatibility"]
+    testing_types = ["functional", "usability", "compatibility", "negative", "positive"]
     columns = ["number", "short description", "priority", "testing type", "steps", "expected results"]
-    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Outcome should be ready for instant convertation to the csv file. Don't add any other text. Use different testing types, for example {testing_types}. Columns: {columns}. One step is one action."
+    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Outcome should be ready for instant convertation to the csv file. Don't add any other text. Use different testing types, for example {testing_types}. Use columns {columns}. One line is one test case."
 
     chat_response = client.chat(
         model=model,
@@ -25,7 +25,7 @@ def main():
     )
     cases = chat_response.choices[0].message.content[1:-1]  # remove " symbols
     print(cases)
-    with open("test_cases.csv", "w") as file:
+    with open("test_cases_prompt.csv", "w") as file:
         file.write("sep=,\n")  # User-friendly Excel
         file.write(cases)
 
