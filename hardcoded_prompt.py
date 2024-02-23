a8ee3dbcd75495c91caace12d10da2e57386b3f0
Ivan Osipov <ivan.osipov@valagroup.com>
2024-02-23 13:22:01 +0200

    Improve results in the prompt only case

--- hardcoded_prompt.py
+++ hardcoded_prompt.py
@@ -16,17 +16,20 @@ def main():
     sut = "e-mail restore"
     sut_elements = ["email field", "send button"]
     testing_types = ["functional", "usability", "compatibility", "negative", "positive"]
-    columns = ["number", "short description", "priority", "testing type", "steps", "expected results"]
-    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Outcome should be ready for instant convertation to the csv file. Don't add any other text. Use different testing types, for example {testing_types}. Use columns {columns}. One line is one test case."
+    columns = ["Number", "Priority", "Description", "Testing type", "Steps", "Expected results"]
+    prompt = f"Write a list of test cases for {sut} with {sut_elements}. Outcome should be ready for instant convertation to the csv file with ; as a separator. Don't add any other text. Use different testing types, such as, but not limited to, {testing_types}. Use columns {columns}. One line is one test case, don't add extra blank lines. DO NOT add testing type in the Description column. In Steps, each step is one numbered action (1, 2, etc), separate every step with a comma ONLY, don't add ; sign there. In Expected results column, text for each case should be splitted and not numbered."
 
     chat_response = client.chat(
         model=model,
         messages=[ChatMessage(role="user", content=prompt)],
     )
-    cases = chat_response.choices[0].message.content[1:-1]  # remove " symbols
-    print(cases)
+    cases = chat_response.choices[0].message.content
+    if cases[0] == '"':
+        cases = cases[1:-1]# remove " symbols
+
+#    print(cases)
     with open("test_cases_prompt.csv", "w") as file:
-        file.write("sep=,\n")  # User-friendly Excel
+        file.write("sep=;\n")  # User-friendly Excel
         file.write(cases)
 
 if __name__ == "__main__":
--- prd_requirements.py
+++ prd_requirements.py
@@ -6,7 +6,7 @@ from mistralai.models.chat_completion import ChatMessage
 
 
 def main():
-    doc = fitz.open('requirements.pdf')  # also give user stories.
+    doc = fitz.open('requirements.pdf')
     reqs = ""
     for page in doc: 
         text = page.get_text() 
@@ -19,7 +19,7 @@ def main():
     client = MistralClient(api_key=api_key)
     testing_types = ["functional", "usability", "compatibility", "negative", "positive"]
     columns = ["number", "short description", "priority", "testing type", "steps", "expected results"]
-    prompt = f"Write a list of test cases based on requirements {reqs}. Outcome should be ready for instant convertation to the csv file. Don't add any other text. Use different testing types, for example {testing_types}. Use columns {columns}. One line is one test case."
+    prompt = f"Write a list of test cases based on requirements {reqs}, but not limited to them. Outcome should be ready for instant convertation to the csv file with ; as a separator. Don't add any other text. Use different testing types, for example {testing_types}. Use columns {columns}. One line is one test case, don't add extra blank lines."
 
     chat_response = client.chat(
         model=model,
@@ -28,7 +28,7 @@ def main():
     cases = chat_response.choices[0].message.content[1:-1]  # remove " symbols
     print(cases)
     with open("test_cases_reqs.csv", "w") as file:
-        file.write("sep=,\n")  # User-friendly Excel
+        file.write("sep=;\n")  # User-friendly Excel
         file.write(cases)
 
 if __name__ == "__main__":
