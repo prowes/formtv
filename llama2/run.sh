3ea3791d461701a5a12c81c1af73f531f071d74c
Ivan Osipov <ivan.osipov@valagroup.com>
2024-02-28 15:01:25 +0200

    Update path

--- llama2/run.sh
+++ llama2/run.sh
@@ -1,4 +1,4 @@
-torchrun --nproc_per_node 2 example_chat_completion.py \
+torchrun --nproc_per_node 2 /home/iv/llama/example_chat_completion.py \
     --ckpt_dir /home/iv/llama/llama-2-13b-chat/ \
     --tokenizer_path /home/iv/llama/tokenizer.model \
     --max_seq_len 512 --max_batch_size 6
