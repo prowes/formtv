a3b0389616531f4cf7b5cfd4a76030ba7e57dac1
Ivan Osipov <ivan.osipov@valagroup.com>
2024-03-01 17:00:23 +0200

    Change slashes

--- llama2/pdf_requirements.py
+++ llama2/pdf_requirements.py
@@ -3,7 +3,7 @@ import fitz
 
 
 def main():
-    doc = fitz.open('..\\resources\\requirements.pdf')
+    doc = fitz.open('..//resources//requirements.pdf')
     reqs = ""
     for page in doc:
         text = page.get_text()
@@ -14,7 +14,7 @@ def main():
     columns = "Number, Priority, Description, Testing type, Steps, Expected results"
     prompt = f"You are a QA Engineer. Write a list of test cases based on requirements {reqs}, but not limited to them. Outcome should be ready for instant converting to the csv file with semicolon as a separator. Do not add any other text. Use different testing types, such as, but not limited to, {testing_types}. Use columns {columns}. One line is one test case, do not add extra blank lines. DO NOT add testing type in the Description column. In Steps, each step is one numbered action like 1, 2, etc, separate every step with a comma ONLY, do not add semicolon sign there. In Expected results column, text for each case should be splitted and not numbered. Test cases numeration should be like 1, 2, 3 etc"
 
-    os.system(f'ollama run codellama:7b-instruct {prompt} > results\test_use_cases.csv')
+    os.system(f'ollama run codellama:7b-instruct {prompt} > results/test_use_cases.csv')
 
 if __name__ == "__main__":
     main()
