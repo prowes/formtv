c4c8df3b528112f9b8d6df0415ac585146df4555
Ivan Osipov <ivan.osipov@valagroup.com>
2024-03-06 15:08:05 +0200

    from sh to py

--- llama2/llama_pdf_usecases.py
+++ llama2/llama_pdf_usecases.py
@@ -1,5 +1,6 @@
 import os
 import fitz
+import prompts
 
 
 def main():
@@ -10,9 +11,7 @@ def main():
         text = ' '.join(text.split())
         usecases += text
 
-    testing_types = "functional, usability, compatibility, negative"
-    columns = "Number, Priority, Description, Testing type, Steps, Expected results"
-    prompt = f"You are a QA Engineer. Write a list of test cases based on use cases {usecases}. Outcome should be ready for instant converting to the csv file with semicolon as a separator. Do not add any other text. Use different testing types, such as, but not limited to, {testing_types}. Use columns {columns}. One line is one test case, do not add extra blank lines. DO NOT add testing type in the Description column. In Steps, each step is one numbered action like 1, 2, etc, separate every step with a comma ONLY, do not add semicolon sign there. In Expected results column, text for each case should be splitted and not numbered. Test cases numeration should be like 1, 2, 3 etc"
+    prompt = f"Use cases: {usecases}. {prompts.prompt_sample}"
     #print(prompt)
     os.system(f'ollama run codellama:7b-instruct {prompt} > results/test_use_cases.csv')
     #os.system(f'"{prompt}" > test_use_cases.csv')
