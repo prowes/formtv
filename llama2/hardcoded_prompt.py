29489562c78b41726d9d2c994eb7041bb3e2d56d
Ivan Osipov <ivan.osipov@valagroup.com>
2024-03-06 15:11:56 +0200

    from sh to py

---
+++ llama2/hardcoded_prompt.py
@@ -0,0 +1,10 @@
+import os
+import prompts
+
+
+def main():
+    prompt = f"{prompts.sut}. {prompts.sut_elements} {prompts.prompt_sample}"
+    os.system(f'ollama run codellama:7b-instruct {prompt} > results/test_reqs.csv')
+
+if __name__ == "__main__":
+    main()
