719f8d75cbe4a8db82d6839cb187bf658ab8bb09
Ivan Osipov <ivan.osipov@valagroup.com>
2024-03-13 11:33:51 +0200

    add a test plan

--- claude/hardcoded_prompt.py
+++ claude/hardcoded_prompt.py
@@ -5,12 +5,11 @@ import anthropic
 
 def main():
     client = anthropic.Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
-    #prompt = f"{prompts.sut}. {prompts.prompt_sample}"
-    prompt = f"You are a part of the team who is going to try different available modern AI models to come up with an AI-based solution for the finnish MTV Katsomo company. The solution is intended to automate the daily routine of QA engineers. It should generate test cases based on the software description, design drafts, use cases and other documents. Your purpose right now is to write an introduction for the document with the description of this task, this product and all the comparison, selection and scripts development process. The instruction should fit one A4 page in Word."
+    prompt = f"{prompts.sut}. {prompts.prompt_sample}"
 
     message = client.messages.create(
         model="claude-3-opus-20240229",
-        max_tokens=2048,
+        max_tokens=3072,
         messages=[
             {"role": "user", "content": prompt}
         ]
@@ -18,7 +17,7 @@ def main():
     cases = message.content[0].text
     print(cases)
 
-    with open("results\\desc.txt", "w") as file:
+    with open("results\\test_cases_prompt.csv", "w") as file:
         file.write("sep=;\n")  # User-friendly Excel
         file.write(cases)
 
--- mistral/hardcoded_prompt.py
+++ mistral/hardcoded_prompt.py
@@ -9,7 +9,8 @@ def main():
     api_key = os.environ["MISTRAL_API_KEY"]
     model = "mistral-medium"  # the best one.
     client = MistralClient(api_key=api_key)
-    prompt = f"{prompts.sut}. {prompts.prompt_sample}"
+    #prompt = f"{prompts.sut}. {prompts.prompt_sample}"
+    prompt = "Please give me a test plan for the login screen"
 
     chat_response = client.chat(
         model=model,
@@ -20,8 +21,9 @@ def main():
         cases = cases[1:-1]# remove " symbols
 
     print(cases)
-    with open("results\\test_cases_prompt.csv", "w") as file:
-        file.write("sep=;\n")  # User-friendly Excel
+    #with open("results\\test_cases_prompt.csv", "w") as file:
+    with open("results\\test_plan.txt", "w") as file:
+     #   file.write("sep=;\n")  # User-friendly Excel
         file.write(cases)
 
 if __name__ == "__main__":
